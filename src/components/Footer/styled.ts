78c43275d0e313b37c676eeba6211b400f686b33
Gustavo Theotonio <gustavotheotonio46@gmail.com>
2023-02-26 14:13:50 -0300

    update footer

--- src/components/Footer/styled.ts
+++ src/components/Footer/styled.ts
@@ -7,6 +7,6 @@ export const FooterContainer = styled.footer`
   margin-bottom: 1rem;
 
   span {
-    color: ${(props) => props.theme.colors.text};
+    color: ${(props) => props.theme.colors.text_label};
   }
 `
